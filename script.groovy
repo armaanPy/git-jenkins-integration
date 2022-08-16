a37bd09e8f20b52aefe67bc030094cc39a1bff52
armaanPy <52916251+armaanPy@users.noreply.github.com>
2022-08-16 15:21:14 +0100

    Multiple try excepts for auditTools()

--- script.groovy
+++ script.groovy
@@ -14,13 +14,28 @@ def auditTools() {
     try {
     sh '''
     lsb_release -a
+    '''
+    } catch(Exception e) {
+        println("Exception: ${e}")
+    }
+
+    try {
+    sh '''
     git version
+    '''
+    } catch(Exception e) {
+        println("Exception: ${e}")
+    }
+    
+    try {
+    sh '''
     docker version
     '''
     } catch(Exception e) {
         println("Exception: ${e}")
     } finally {
-        // The finally block always executes.
+        println("Continuing step...")
+        // The finally block always executes
     }
 }
 
